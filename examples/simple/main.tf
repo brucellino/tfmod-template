284589986f3eb13249266830a78b70cc62257f35
Bruce Becker <brucellino@protonmail.com>
2022-06-24 10:24:34 +0200

    feat(main): add initial definition of module
    Signed-off-by: Bruce Becker <brucellino@protonmail.com>

---
+++ examples/simple/main.tf
@@ -0,0 +1,6 @@
+# This is the default example
+# customise it as you see fit for your example usage of your module
+
+module "example" {
+  source = "../../"
+}
---
+++ terraform.tf
@@ -0,0 +1,12 @@
+# Use this file to declare the terraform configuration
+# Add things like:
+# - required version
+# - required providers
+# Do not add things like:
+# - provider configuration
+# - backend configuration
+# These will be declared in the terraform document which consumes the module.
+
+terraform {
+  required_version = ">1.2.0"
+}
